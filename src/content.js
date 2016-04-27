9e16bc7528ec7fb4cef3a46f8693f2e70625d085
abukurov <abukurov@hwdtech.ru>
2016-04-27 13:54:34 +0600

    Reuse VK message markup

--- src/content.js
+++ src/content.js
@@ -1,18 +1,18 @@
 import PageObserver from './observer';
 import { PAGE_URL_UPDATED_EVENT } from './events';
 
-const FAKE_MESSAGE_INNER_TPL =  [
-  '<td></td>',
-  '<td class="fake-message__image-wrapper">',
-  '<img class="fake-message__image" src="' + chrome.extension.getURL('/images/icon_48.png') + '"/>',
-  '</td>',
-  '<td class="fake-message__text" colspan="3">Прилетело НЛО и удалило эту запись</td>'
-].join('');
+const AVATAR_URL = chrome.extension.getURL('/images/icon_48.png');
+const DEFAULT_MESSAGE = 'Прилетело НЛО и удалило эту запись';
 
 function transformMessageElement(element) {
-  element.style.setProperty('height', element.clientHeight + 'px');
-  element.classList.add('fake-message');
-  element.innerHTML = FAKE_MESSAGE_INNER_TPL;
+  let messageAttachmentEl = element.querySelector('.wall_module');
+
+  element.querySelector('.im_log_author_chat_thumb img').setAttribute('src', AVATAR_URL);
+  element.querySelector('.im_log_author_chat_name a').textContent = 'Slowpoke';
+  element.querySelector('.im_msg_text').textContent = DEFAULT_MESSAGE;
+
+  //Remove message attachments
+  messageAttachmentEl && (messageAttachmentEl.innerHTML = '');
 }
 
 function transformNewVkMessageElement(element) {
@@ -24,10 +24,11 @@ function transformNewVkMessageElement(element) {
   element.style.height = '55px';
 
   const contentEl = element.querySelector('.im-mess-stack--content');
-  contentEl.innerHTML = '<strong style="color: #343434; display: block; padding-top: 21px">Прилетело НЛО и удалило эту запись</strong>';
+
+  contentEl.innerHTML = `<strong style="color: #343434; display: block; padding-top: 21px">${DEFAULT_MESSAGE}</strong>`;
   contentEl.style.marginLeft = '95px';
 
-  element.querySelector('.im-mess-stack--photo img').src = chrome.extension.getURL('/images/icon_48.png');
+  element.querySelector('.im-mess-stack--photo img').setAttribute('src', AVATAR_URL);
 }
 
 function createTransformer(isNewVersion) {
