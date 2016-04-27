8d1423e2fe34bdd5d810adc095329d8cba7b1008
Maxim Malov <m.malov.v@gmail.com>
2016-04-27 22:12:21 +0600

    Reuse new vk design markup

--- src/content.js
+++ src/content.js
@@ -3,12 +3,13 @@ import { PAGE_URL_UPDATED_EVENT } from './events';
 
 const AVATAR_URL = chrome.extension.getURL('/images/icon_48.png');
 const DEFAULT_MESSAGE = 'Прилетело НЛО и удалило эту запись';
+const REPLACE_NAME = 'Slowpoke';
 
 function transformMessageElement(element) {
   let messageAttachmentEl = element.querySelector('.wall_module');
 
   element.querySelector('.im_log_author_chat_thumb img').setAttribute('src', AVATAR_URL);
-  element.querySelector('.im_log_author_chat_name a').textContent = 'Slowpoke';
+  element.querySelector('.im_log_author_chat_name a').textContent = REPLACE_NAME;
   element.querySelector('.im_msg_text').textContent = DEFAULT_MESSAGE;
 
   //Remove message attachments
@@ -21,14 +22,15 @@ function transformNewVkMessageElement(element) {
     return;
   }
 
-  element.style.height = '55px';
-
-  const contentEl = element.querySelector('.im-mess-stack--content');
-
-  contentEl.innerHTML = `<strong style="color: #343434; display: block; padding-top: 21px">${DEFAULT_MESSAGE}</strong>`;
-  contentEl.style.marginLeft = '95px';
-
   element.querySelector('.im-mess-stack--photo img').setAttribute('src', AVATAR_URL);
+  element.querySelector('.im-mess-stack--pname .im-mess-stack--lnk').textContent = REPLACE_NAME;
+
+  let messageTextEl = element.querySelector('.im-mess--text .im_msg_text');
+  messageTextEl.textContent = DEFAULT_MESSAGE;
+
+  let messageAttachmentEl = messageTextEl.nextElementSibling;
+  //Remove message attachments
+  messageAttachmentEl && (messageAttachmentEl.innerHTML = '');
 }
 
 function createTransformer(isNewVersion) {
